2fe834b88355ebc403e628db861e432adc93eb0f
itaya koshiro <happy4ro@gmail.com>
2022-07-13 09:40:23 +0900

    Listviewの追加、編集画面作成途中

--- app/src/main/java/jp/ac/gifu_u/last/Menu.java
+++ app/src/main/java/jp/ac/gifu_u/last/Menu.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +24,30 @@ public class Menu extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         list = getIntent().getStringArrayListExtra("key1");
+
+        List<List<String>> list4 = new ArrayList<>();
+        List<String> in_list = new ArrayList<String>();
+
+        in_list.add("1000");
+        in_list.add("aaa");
+
+        list4.add(in_list);
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list4);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(Menu.this, "ok", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
 
         TextView textview3 = findViewById(R.id.textView3);
         textview3.setText(list.get(0));
