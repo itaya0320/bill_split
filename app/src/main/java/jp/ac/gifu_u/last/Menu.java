f66f4012bfb1f676c2560123e5805c34e5276438
itaya koshiro <happy4ro@gmail.com>
2022-07-27 12:15:12 +0900

    計算単位変更

--- app/src/main/java/jp/ac/gifu_u/last/Menu.java
+++ app/src/main/java/jp/ac/gifu_u/last/Menu.java
@@ -1,14 +1,17 @@
 package jp.ac.gifu_u.last;
 
+import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +23,7 @@ public class Menu extends AppCompatActivity{
 
     private List<String> viewdatas = new ArrayList<>();
     private List<Movemoney> list_movemoney = new ArrayList<>();
+    private List<List <Movemoney>> all_movemoney = new ArrayList<>();
     private List<String> viewdatas2 = new ArrayList<>();
 
 
@@ -57,16 +61,42 @@ public class Menu extends AppCompatActivity{
             calculate();
         }
 
+        ArrayAdapter<String> money_round = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item);
+        money_round.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        money_round.add("1");
+        money_round.add("10");
+        money_round.add("100");
+        Spinner spinner = (Spinner) findViewById(R.id.money_round);
+        spinner.setAdapter(money_round);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            // アイテムが選択された時の動作
+            public void onItemSelected(AdapterView parent, View view, int position, long id) {
+                try {
+                    List<Movemoney> move_money_list = new ArrayList<>();
+                    move_money_list = all_movemoney.get(position);
+                    viewdatas2.clear();
+                    for (int i = 0; i < move_money_list.size(); i++) {
+                        viewdatas2.add(move_money_list.get(i).lower + "が" + move_money_list.get(i).upper + "に" + move_money_list.get(i).balance + "円払う");
+                    }
+                    newAdapter_Create();
+                } catch (Exception e){
+                    Log.d("tag",String.valueOf(all_movemoney.size()));
+                }
 
-        for(int i = 0 ; i < list_movemoney.size() ; i++) {
-            viewdatas2.add(list_movemoney.get(i).lower + "が" + list_movemoney.get(i).upper + "に" + list_movemoney.get(i).balance +"円払う");
-        }
+            }
 
+            // 何も選択されなかった時の動作
+            public void onNothingSelected(AdapterView parent) {
+            }
+        });
+    }
+
+    private void newAdapter_Create(){
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,viewdatas2);
         ListView resultlist = (ListView)findViewById(R.id.resultlist);
         resultlist.setAdapter(adapter2);
-
     }
+
 
     private void calculate(){
         int cnt = 0;
@@ -75,6 +105,7 @@ public class Menu extends AppCompatActivity{
         Calculatedata calculatedata = new Calculatedata();
         List<Calculatedata> all = new ArrayList<>();
         Movemoney movemoney = new Movemoney();
+        Spinner resultlist = (Spinner)findViewById(R.id.money_round);
         for(int i = 0; i < Register.people.size() ; i++){
             for(int j = 0; j < Register.data.size() ; j ++){
                 if (Register.data.get(j).name == Register.people.get(i)){
@@ -105,6 +136,17 @@ public class Menu extends AppCompatActivity{
             list_movemoney.add(movemoney);
             movemoney = new Movemoney();
         }
-
+        for(int i = 0; i < 3; i++) {
+            ArrayList<Movemoney> list_movemoneyRound = new ArrayList<>();
+            for (int j = 0; j < list_movemoney.size(); j++) {
+                int balance_new = (int) (Math.round((double) list_movemoney.get(j).balance / Math.pow(10, i)) * Math.pow(10, i));
+                movemoney = new Movemoney();
+                movemoney.upper = list_movemoney.get(j).upper;
+                movemoney.lower = list_movemoney.get(j).lower;
+                movemoney.balance = balance_new;
+                list_movemoneyRound.add(movemoney);
+            }
+            all_movemoney.add(list_movemoneyRound);
+        }
     }
 }
